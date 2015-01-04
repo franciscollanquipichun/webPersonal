874ecd014bbd729a9aa5f815717cf817f73584f7
Francisco Javier Llanquipichun Garcia <francisco.llanquipichun@gmail.com>
2015-01-04 00:50:16 -0300

    firs commit
    First commit!!

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Francisco Llanquipichun on 03-01-15.
+ */
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    openFollowMe();
+    openContact();
+    openContactForm()
+});
+
+function openFollowMe() {
+    $( "#follow-btn" ).click(function() {
+        $("#contact").hide();
+        $( "#follow" ).toggle("fast");
+    });
+}
+function openContact() {
+    $( "#contact-btn" ).click(function() {
+        $( "#follow" ).hide();
+        $("#contact").toggle("fast");
+    });
+}
+
+function openContactForm() {
+    $( "#mail" ).click(function() {
+        $("#contact-form").toggle("fast");
+        textcount();
+    });
+}
+
+function textcount() {
+    $("#message").keyup(function(){
+        var ms = $(this).val();
+        $( "#text-count" ).html(ms.length+":255");
+    });
+}
