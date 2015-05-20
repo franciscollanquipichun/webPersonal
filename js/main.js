99d659ff649918baab1f0c90910f98173584f66d
Francisco Javier Llanquipichun Garcia <francisco.llanquipichun@gmail.com>
2015-05-20 01:30:46 -0300

    Habilitado envio de correo
    Agregado formulario de contacto funciona, validado por captcha.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ $( document ).ready(function() {
     openFollowMe();
     openContact();
     openContactForm();
+
+    $('#contact-form').on('submit', function (e) {
+        sendAjaxForm();
+        e.preventDefault();
+        //OR
+        return false;
+    });
+
 });
 
 function openFollowMe() {
@@ -33,4 +41,25 @@ function textcount() {
         var ms = $(this).val();
         $( "#text-count" ).html(ms.length+":255");
     });
+}
+
+function sendAjaxForm() {
+
+    var form_data = {
+        'captcha'   : $('.g-recaptcha-response').val(),
+        'name'      : $('input[name=nombre]').val(),
+        'mail'      : $('input[name=mail]').val(),
+        'msg'       : $('textarea[name=text]').val()
+    };
+
+    $.ajax({
+        url: 'mailto.php',
+        type:'POST',
+        data: form_data,
+        success: function (response) {
+            //examina la respuesta
+            console.log(response);
+        }
+    });
+
 }
--- mailto.php
+++ mailto.php
@@ -6,54 +6,51 @@
  * Time: 21:55
  */
 
-$msg = '';
-$recaptcha = $_POST['g-recaptcha-response'];
+$captcha_data = $_POST['captcha'];
+$name = $_POST['name'];
+$email = $_POST['mail'];
+$msg = $_POST['msg'];
 
-if(!empty($recaptcha))
-{
-    include("getCurlData.php");
+/**
+ * validacion de captcha
+ */
+if(!empty($captcha_data)) {
+
     $google_url = "https://www.google.com/recaptcha/api/siteverify";
     $secret = '6LcbqwATAAAAAGcAaUkYfDelgh0WqgjIby4EYVkO';
     $ip = $_SERVER['REMOTE_ADDR'];
-    $url = $google_url."?secret=".$secret."&response=".$recaptcha."&remoteip=".$ip;
+    $url = $google_url."?secret=".$secret."&response=".$captcha_data."&remoteip=".$ip;
 
-    $msg = http_get($url);
-    echo($msg);
-/*
-    $res = getCurlData($url);
-    $res = json_decode($res, true);
+    $response = file_get_contents($url);
+    $aceptado = json_decode($response);
 
-    if($res['success']) {
+    //valida el capcha
+    if($aceptado->success == TRUE ) {
 
-            $nombre = $_POST['nombre'];
-            $mail = $_POST['mail'];
-            $text = $_POST['text'];
+        if($name !== '' && $email !== '' && $msg !== '') {
 
-            if($nombre !== '' && $mail !== '' && $text !== '') {
+            $email_to = "francisco.llanquipichun@gmail.com";
+            $email_subject = "Contacto desde el sitio web";
 
-                $email_to = "francisco.llanquipichun@gmail.com";
-                $email_subject = "Contacto desde el sitio web";
+            $email_message = "Contacto por correo desde francisco.llanquipichun.cl:\n\n";
+            $email_message .= "Nombre: " . $name . "\n";
+            $email_message .= "E-mail: " . $email . "\n";
+            $email_message .= "Mensaje:\n" . $msg . "\n";
 
-                $email_message = "Contacto por correo desde francisco.llanquipichun.cl:\n\n";
-                $email_message .= "Nombre: " . $nombre . "\n";
-                $email_message .= "E-mail: " . $mail . "\n";
-                $email_message .= "Comentarios: " . $text . "\n\n";
+            $headers = 'From: francisco@llanquipichun.cl'."\r\n".
+                'Reply-To: francisco@llanquipichun.cl'."\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+            @mail($email_to, $email_subject, $email_message, $headers);
 
-                $headers = 'From: francisco.llnquipichun@gmail.com'."\r\n".
-                    'Reply-To: francisco.llnquipichun@gmail.com'."\r\n" .
-                    'X-Mailer: PHP/' . phpversion();
-                @mail($email_to, $email_subject, $email_message, $headers);
+            echo "¡El formulario se ha enviado con éxito!";
+        }else {
+            echo('no se ha podido enviar el mensaje: '.$name.' '.$email.' '.$msg);
+        }
 
-                echo "¡El formulario se ha enviado con éxito!";
-            }else {
-                $msg = "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />"
-                      ."Por favor, vuelva atrás y verifique la información ingresada<br />";
-            }
-
-    }else {
-        $msg.="Please re-enter your reCAPTCHA.";
+    }else{
+        echo('Tu eres un robot... no me engañas');
     }
-*/
-}
 
-echo($msg);
+}else{
+    echo('no hay captcha');
+}
